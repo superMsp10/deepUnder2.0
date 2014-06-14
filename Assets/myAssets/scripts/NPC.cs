2b5832357ab7fddb8ff84d1f68241b877743cefd
superMsp10 <pandeymahan34@gmail.com>
2014-06-14 13:17:56 -0700

    worked on bob
    made  bob a NPC with a mob and NPC class. The NPC class is derived from
    the mob class. It is sopposed to show a line of text when the plsyer is
    near.

---
+++ Assets/myAssets/scripts/NPC.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC : Mob
+{
+		public bool blab;
+		public string greeting1 = "Hello";
+		public TextMesh thisText;
+
+		void Start ()
+		{
+				thisText = GetComponentInChildren<TextMesh> ();
+				if (thisText == null) {
+						Debug.LogError ("no text mesh included in NPC");
+				} else if (blab) {
+						thisText.text = greeting1;
+				} else {
+						thisText.text = "";
+				}
+	
+		}
+
+		void Update ()
+		{
+	
+		}
+
+		public void OnTriggerEnter2D ()
+		{
+				Debug.Log ("hi");
+				if (!blab)
+						thisText.text = greeting1;
+		}
+
+
+}
