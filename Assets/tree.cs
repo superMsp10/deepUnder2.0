ec32c636d277eee220a5861ff89b9e8e29c02327
superMsp10 <pandeymahan34@gmail.com>
2014-10-07 21:46:29 -0400

    Mo'Trees
    Changed the way tree class works, it is a part handler rather than the
    part it self. It creates a random (limited by a specified number) amount
    of a chosen(from a specified array of game parts) part at random
    position from the object and a random rotation.
    
    The rotation is chosen before the instaintian using random limited
    nubers using
    
    Quaternion.Euler (0, 0, 10);

--- Assets/tree.cs
+++ Assets/tree.cs
@@ -5,25 +5,27 @@ public class tree : MonoBehaviour
 {
 	 
 		public GameObject[] choices;
-		public GameObject thisPart;
-		public int rotation;
-		public bool autoRotate;
+		public GameObject[] thisParts;
+		public int multi;
+		public float smooth = 2.0F;
+		public float tiltAngle = 30.0F;
+		//public int xMax;
+		//public int yMax;
+		public int maxRot;
+		Quaternion rot;
 
 		public void growTree ()
 		{
-				if (autoRotate) {
-						rotation = Random.Range (0, 360);
-				}
-			
 				if (choices.Length > 0) {
-						thisPart = choices [Random.Range (0, choices.Length)];
-						Instantiate (thisPart, transform.position, Quaternion.identity);
-						transform.rotation.Set (0, 0, rotation, 0); 
-						
+						multi += Random.Range (2, multi);
+						for (int i = 0; i < multi; i++) {
+								GameObject thisChoice = choices [Random.Range (0, choices.Length)];
+								Vector3 pos = new Vector3 (transform.position.x, transform.position.y);
+								rot = Quaternion.Euler (0, 0, 10);
+								Instantiate (thisChoice, pos, rot);
+						}
 				} else
-						Debug.Log ("no cjoices available");
-			
+						Debug.LogError ("no choices for tree parts");
+
 		}
-	
-	
 }
