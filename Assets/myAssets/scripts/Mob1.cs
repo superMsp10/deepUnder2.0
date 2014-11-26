5074f2ade5659834636bf8c7b8b1a011062d6ba2
superMsp10 <pandeymahan34@gmail.com>
2014-11-25 19:22:30 -0500

    A*path
    decided not to use A* path finding becuse too complicated

--- Assets/myAssets/scripts/Mob1.cs
+++ Assets/myAssets/scripts/Mob1.cs
@@ -1,7 +1,30 @@
-﻿using UnityEngine;
+using  UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.Serialization;
+
+
 
 public class Mob1 : Entity {
+	public List<BodyParts>parts;
+	public float HP;
+	public int Dmg;
+	public int blunt, point, slash;
 
+	public void takeDmg(int damage, string type){
+		if(type=="blunt"){
+			HP -=(damage-blunt);
+
+		}else if(type=="point"){
+			HP-=(damage-point)/2;
+			
+		}else if(type=="slash"){
+			int preDmg =0;
+			preDmg=(Random.Range(0,damage)/(slash*Random.Range(-2,slash)));
+			HP-=(preDmg+Random.Range(-slash,slash));
+			
+		}
+
+
+	}
 
 }
