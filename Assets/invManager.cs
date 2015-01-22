478f5c4c650d42789502d37afacef7b6051865ae
superMsp10 <pandeymahan34@gmail.com>
2015-01-21 19:58:18 -0500

    inventory system
    added inventory system for the people

---
+++ Assets/invManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class invManager : MonoBehaviour
+{
+
+		private gameManager thismanage;
+		public List<Image>slots;
+		public Sprite empty;
+		void Start ()
+		{
+				thismanage = gameManager.thisM;
+				foreach (Image s in slots) {
+						s.sprite = empty;
+				}
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
