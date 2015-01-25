b07af731c4fd3b27870fa8de1e186628699cd864
superMsp10 <pandeymahan34@gmail.com>
2015-01-24 20:00:21 -0500

    ui system
    worked on the player system. fnd make sounds for explosion and make it a
    holable

---
+++ Assets/myAssets/scripts/Holdable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Holdable : MonoBehaviour
+{
+
+		public Sprite holdUI;
+
+		public void  onUse ()
+		{
+
+				Debug.Log ("hi");
+		}
+}
--- Assets/myAssets/scripts/invManager.cs
+++ Assets/myAssets/scripts/invManager.cs
@@ -2,23 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 
+
 public class invManager : MonoBehaviour
 {
 
 		private gameManager thismanage;
 		public List<Image>slots;
 		public Sprite empty;
+		public List <Holdable> inv;
 		void Start ()
 		{
 				thismanage = gameManager.thisM;
-				foreach (Image s in slots) {
-						s.sprite = empty;
-				}
+				
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
 	
+		}
+
+		public void changeSlot (Holdable h)
+		{
+				for (int i = 0; i <  inv.Count; i ++) {
+
+						if (inv [i] == null) {
+								inv [i] = h;
+								slots [i].sprite = h.holdUI;
+								return;
+						}
+				}
 		}
 }
