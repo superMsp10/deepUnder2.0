f49c49b3db67e11c95d9e127bdea3b305e2911ec
superMsp10 <pandeymahan34@gmail.com>
2015-03-16 12:31:48 -0400

    npc talk
    worked on npc ability to speak

--- Assets/myAssets/Mobs/Oldman/NPC.cs
+++ Assets/myAssets/Mobs/Oldman/NPC.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class NPC : Mob1
 {
@@ -7,10 +8,35 @@ public class NPC : Mob1
 		public GameObject Target;
 		public List<Quest> quests;
 		public Quest currQuest;
+		public LayerMask whatPlayer;
+		public Text talk;
+		public GameObject responses;
+		public Button  answer1;
+		public Button  answer2;
 		
 		public virtual void PlayerInteract ()
 		{
 				Debug.Log ("hi");
+		}
+		void Update ()
+		{
+				if (animated)
+						updateAnim ();
+				checkDead ();
+				checkFacing ();
+		}
+		void OnTriggerEnter2D (Collider2D other)
+		{
+			
+				if (other.gameObject.tag == "Player")
+						responses.SetActive (true);
+		}
+		
+		void OnTriggerExit2D (Collider2D other)
+		{
+		
+				if (other.gameObject.tag == "Player")
+						responses.SetActive (false);
 		}
 
 }
--- Assets/myAssets/Mobs/Oldman/OldMan.cs
+++ Assets/myAssets/Mobs/Oldman/OldMan.cs
@@ -1,13 +1,9 @@
 using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
 
 public class OldMan : NPC
 {
-		public Text talk;
-		public GameObject responses;
-		public Button  answer1;
-		public Button  answer2;
+		
 
 		
 }
