449a212a875c803c1852a88d011984d859f53a2c
superMsp10 <pandeymahan34@gmail.com>
2015-03-19 09:04:13 -0400

    finished stack
    the staking mechanics are finished teh stuffs stck properly

--- Assets/myAssets/scripts/slotCollection.cs
+++ Assets/myAssets/scripts/slotCollection.cs
@@ -23,24 +23,41 @@ public class slotCollection : MonoBehaviour
 		{
 				int returning = 0;
 				for (int i = 0; i <  slots.Count; i ++) {
-						if (slots [i].holding == null) {
-								Debug.Log ("hi");
+
+						if (slots [i].holding == null || slots [i].holding == h && slots [i].amount < h.stackSize) {
+								int left = slots [i].amount + amount;
+								if (left > h.stackSize) {
+										amount = left - h.stackSize;
+										slots [i].changeHolding (h, h.stackSize);
+								} else {
+										slots [i].changeHolding (h, left);
+										return 0; 
+										amount = 0;
+								}
+						}
+						/*	if (slots [i].holding == null) {
+							
 
 								if (amount > h.stackSize) {
-										returning = amount - h.stackSize;
+										slots [i].changeHolding (h, h.stackSize);
+										amount = amount - h.stackSize;
+
 								} else {
 										slots [i].changeHolding (h, amount);
 										return 0;
+
 								}
 						} else if (slots [i].holding == h && slots [i].amount < h.stackSize) {
+								int space = h.stackSize - slots [i].amount;
 
+								slots [i].changeHolding (h, amount + slots [i].amount);
 
 						}
+				}*/
+
 				}
-
-				return 0;
+				return returning;
 		}
-
 
 		public void changeSlot (Holdable h, int i)
 		{
