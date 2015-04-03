0b92146cfef462182cc4fd55f4f7e38dd0339b1c
superMsp10 <pandeymahan34@gmail.com>
2015-04-03 11:51:54 -0400

    ranach weapon

--- Assets/myAssets/Mobs/Oldman/Ranach.cs
+++ Assets/myAssets/Mobs/Oldman/Ranach.cs
@@ -19,30 +19,59 @@ public class Ranach : NPC
 		
 				if (speakStage == "started") {
 						if (answer) {
-								talk.text = "The old man wants to use you as his weapon";
-								answer1Text.text = "What can i do?";
-								answer2Text.text = "baloney!";
-								speakStage = "plan";
+								tell ();
+								goto end;
 						} else {
 								talk.text = "BOY! trust me that old man is evil";
 								answer1Text.text = "What can i do?";
 								answer2Text.text = "So?";
 								speakStage = "plan";
+								changeTalkBoxState (true);
 
+								goto end;
 						}
 				}
 
 				if (speakStage == "plan") {
-			if(answer)
+						if (answer) {
+								giveWeapon ();
+								speakStage = "done";
+								goto end;
+						} else {
+								talk.text = "Atleast i tried";
+								answer1Text.text = "Bye";
+								answer2Text.text = "and failed....";
+								talking = false;
+								changeTalkBoxState (true);
+
+								speakStage = "done";
+								goto end;
+						}
+
 
 				}
+
+				end:
+				;
 		}
 
+		void tell ()
+		{
+				talk.text = "The old man wants to use you as his weapon";
+				answer1Text.text = "What can i do?";
+				answer2Text.text = "baloney!";
+				speakStage = "plan";
+				changeTalkBoxState (true);
 
+		}
 
 		void giveWeapon ()
 		{
 				talk.text = "Take this and attack the old man when you see him";
 				drop.SetActive (true);
+				answer1Text.text = "ok";
+				answer2Text.text = "maybe";
+				changeTalkBoxState (true);
+
 		}
 }
