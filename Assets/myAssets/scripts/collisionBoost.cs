720dc29761364f488d748057277d98739428534c
superMsp10 <pandeymahan34@gmail.com>
2015-07-29 14:26:19 -0400

    springy

--- Assets/myAssets/scripts/collisionBoost.cs
+++ Assets/myAssets/scripts/collisionBoost.cs
@@ -5,13 +5,18 @@ public class collisionBoost : MonoBehaviour
 {
 
 		public Vector2 dir;
-
+		public bool calcVelo = false;
+		public Transform to;
+		public float toBoost = 1.5f;
 
 		public void  boost (Rigidbody2D target)
 		{
 
-			
-				target.velocity = dir;
+				if (calcVelo) {
+						target.velocity = (to.position - target.transform.position) * toBoost;
+
+				} else
+						target.velocity = dir;
 
 		}
 
