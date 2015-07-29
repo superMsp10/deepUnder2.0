68c1fa1004a96bd9668173d6e8c7c64dd8eda505
superMsp10 <pandeymahan34@gmail.com>
2015-07-29 13:45:25 -0400

    spring
    changed script to set plauer velocity rater than adding velocity

--- Assets/myAssets/scripts/collisionBoost.cs
+++ Assets/myAssets/scripts/collisionBoost.cs
@@ -3,20 +3,15 @@ using System.Collections;
 
 public class collisionBoost : MonoBehaviour
 {
-		public int MultiplierX;
-		public int MultiplierY;
-		public float boostAmount;
-		public float randomrange = 0;
+
 		public Vector2 dir;
-		public Transform loc;
+
 
 		public void  boost (Rigidbody2D target)
 		{
 
-				Vector2 tagetVeloMulti = new Vector2 (MultiplierX, MultiplierY);
-				Vector2 boost = new Vector2 (dir.x * (boostAmount + Random.Range (0, randomrange)), dir.y * (boostAmount + Random.Range (0, randomrange)));
-				Vector2 force = (tagetVeloMulti + boost);
-				target.AddForceAtPosition (force, transform.position);
+			
+				target.velocity = dir;
 
 		}
 
