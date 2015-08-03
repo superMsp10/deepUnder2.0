f8b57e8ddfd8b3c6faf20609762a43512acd766b
superMsp10 <pandeymahan34@gmail.com>
2015-08-02 22:51:35 -0400

    energy ball
    added gravitational atttraction effects to energy ball

---
+++ Assets/energyball.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class energyball : Arrow
+{
+
+		public LayerMask gravityEffect;
+		public int distance;
+		public int force = 1000;
+
+
+		void FixedUpdate ()
+		{
+
+				Collider2D[] hit = Physics2D.OverlapCircleAll (transform.position, distance,
+		                                               gravityEffect);
+
+				foreach (Collider2D c in hit) {
+						Rigidbody2D r = c.rigidbody2D;
+						if (r != null) {
+								r.AddForce ((transform.position - c.transform.position) * (force / Vector2.Distance (transform.position, c.transform.position)));
+						}
+				}
+	
+		}
+}
